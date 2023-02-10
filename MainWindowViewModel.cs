b7849d33b34f940117f6e66bf79ed39d048af2c6
Kazuhiro Takahashi <pupodenews@gmail.com>
2023-02-11 01:54:21 +0900

    ButtonSample

---
+++ MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+
+namespace MyAvoloniaApp;
+
+public class MainWindowViewModel : INotifyPropertyChanged
+{
+    private string _buttonText = "Click Me!";
+
+    public string ButtonText { 
+        get => _buttonText;
+        set 
+        {
+            _buttonText = value;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(ButtonText)));
+        }
+    }
+
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    public void ButtonClicked() => ButtonText = "Hello, Avalonia!";
+}
